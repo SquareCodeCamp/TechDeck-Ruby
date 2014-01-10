32b67b94c66c18c04ff3f909243c3eddc3e6f69e
Square CodeCamp 2013 <codecamp@squareup.com>
2014-01-10 10:11:22 -0800

    Adds rails base

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TechDeckRuby::Application.config.secret_key_base = 'd6da98b49cc06bf9bde55ca566b793311ebead277ca731fbf93cf8f3c8bf94b22715e39e3779307f5e3f7aa24a272ace0609e0eabf7c9d3a0f85fc53ac4b7c0d'
